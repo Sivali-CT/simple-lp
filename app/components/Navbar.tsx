a3b1342af41280506a246428b3c10f0d26b5fe17
Sivali-CT <admin@sivali.co.id>
2023-12-24 16:54:10 +0700

    navigation for mobile edited

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -54,7 +54,7 @@ export default function Navbar({ scrollToSection }: NavbarProps) {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-1/2 overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <button
               type="button"
@@ -69,14 +69,13 @@ export default function Navbar({ scrollToSection }: NavbarProps) {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <button
                     key={item.name}
-                    href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-normal tracking-wider leading-7 text-gray-900 hover:bg-gray-50"
                     onClick={() => handleNavigationClick(item.href.slice(1))}
                     >
                     {item.name}
-                  </a>
+                  </button>
                 ))}
               </div>
             </div>
